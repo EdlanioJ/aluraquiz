5a0060100d8dc0f625477776ea9fb7f664c3df98
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-25 17:24:49 +0100

    add head metatags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
+
 import { ThemeProvider } from 'styled-components';
 
 import db from '../db.json';
@@ -9,6 +11,13 @@ const theme = db.theme;
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>{db.title}</title>
+        <meta property="og:title" content={db.title} />
+
+        <meta property="og:image" content={db.bg} />
+        <meta property="og:image:type" content="image/jpg" />
+      </Head>
       <GlobalStyles />
       <Component {...pageProps} />
     </ThemeProvider>
