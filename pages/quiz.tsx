dad6c45578cf7e9e79d4466747efb84007253c18
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-28 17:45:28 +0100

    create quiz page

--- pages/quiz.tsx
+++ pages/quiz.tsx
@@ -4,9 +4,12 @@ import { useRouter } from 'next/router';
 import styled from 'styled-components';
 import QuizBackground from '../src/components/QuizBackground';
 import db from '../db.json';
-import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
+import QuestionWidget from '../src/pages/Quiz/QuestionWidget';
+import LoadingWidget from '../src/pages/Quiz/LoadingWidget';
+import ResultWidget from '../src/pages/Quiz/ResultWidget';
 
 const QuizContainer = styled.div`
   width: 100%;
@@ -18,32 +21,61 @@ const QuizContainer = styled.div`
     padding: 15px;
   }
 `;
-
+const screenStates = {
+  QUIZ: 'QUIZ',
+  LOADING: 'LOADING',
+  RESULT: 'RESULT',
+};
 export default function Quiz() {
-  const [nameQuiz, setNameQuiz] = useState('');
+  const totalQuestions = db.questions.length;
+  const [results, setResults] = useState<boolean[]>([]);
+  const [screenState, setScreenState] = useState(screenStates.LOADING);
+  const [, setNameQuiz] = useState('');
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const question = db.questions[currentQuestion];
   const router = useRouter();
 
+  function addResult(result: boolean) {
+    setResults([...results, result]);
+  }
   useEffect(() => {
     const { name } = router.query;
-    if (!name) router.push('/');
 
     setNameQuiz(name as string);
   }, []);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setScreenState(screenStates.QUIZ);
+    }, 1 * 1000);
+  }, []);
+
+  function onSubmit() {
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < totalQuestions) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setScreenState(screenStates.RESULT);
+    }
+  }
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
-        <Widget>
-          <Widget.Header>
-            <h1>Página em Construção</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>
-              Olá {nameQuiz}, em breve Poderá dar continuidade a seu jogo,
-              aguarde...
-            </p>
-          </Widget.Content>
-        </Widget>
+        <QuizLogo />
+        {screenState === screenStates.QUIZ && (
+          <QuestionWidget
+            question={question}
+            questionIndex={currentQuestion}
+            totalQuestions={totalQuestions}
+            onSubmit={onSubmit}
+            addResult={addResult}
+          />
+        )}
+        {screenState === screenStates.LOADING && <LoadingWidget />}
+        {screenState === screenStates.RESULT && (
+          <ResultWidget results={results} />
+        )}
         <Footer />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/EdlanioJ/aluraquiz" />
