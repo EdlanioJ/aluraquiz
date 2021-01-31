47f93e9dde89a5d89bb7c09e3c86661af0f308ff
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-31 14:22:17 +0100

    add firebase config

---
+++ src/config/firebase.ts
@@ -0,0 +1,13 @@
+import admin from 'firebase-admin';
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+});
+
+const db = admin.firestore();
+
+export default db;
