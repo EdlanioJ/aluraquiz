912b4b38b4d940646743e56897b70a11981d6eb4
Edlâneo Júlio <edlanioj@gmail.com>
2021-02-01 11:34:33 +0100

    add validation if firebase app is open

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,12 +1,14 @@
 import admin from 'firebase-admin';
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  }),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+  });
+}
 
 const db = admin.firestore();
 
